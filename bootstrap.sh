101a249e803c6c860df0231be9e767963506e74a
Luke <lsh-0@users.noreply.github.com>
2021-01-27 23:43:35 +1030

    bootstrap, purges snap, installs zfsutils

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
-set -e
-echo "hello, world"
+# run as root
+set -euo pipefail
+
+apt-get update
+
+apt-get purge snapd -y
+
+apt-get install \
+    zfsutils-linux \
+    -y --no-install-recommends
+
+printf "\n \n--- this script can be found at: /root/bootstrap.sh\n \n"
